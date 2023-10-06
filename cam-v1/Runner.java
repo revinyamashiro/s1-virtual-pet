63767b9604d6c6e5416b1991c2e5b85a52ce693c
revinyamashiro <revinyamashiro@ahs.local>
2023-10-06 15:03:23 -0700

    dsfasd

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,8 +1,43 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Runner{
+    public Runner(){
+        VirtualPet p = new VirtualPet();
+        p.exercise();
+        takeABeat(1000);
+        p.feed();
+        takeABeat(1000);
+        p.exercise();
+        String ans = getResponse("How was your day?");
+        System.out.println(ans);
+        }
+
+    public String getResponse(String q){
+        String s = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    q,
+                    "Get a Response",
+                    JOptionPane.PLAIN_MESSAGE
+    );
+    return s;
+
+    }
+
+    public void takeABeat(int milliseconds){
+        try {
+            Thread.sleep(1000); //milliseconds
+        } catch(Exception e){
+
+    }
+}
+
+
+
     public static void main(String[] args){
-        VirtualPet v = new VirtualPet();
+        new Runner();
         // v.sleep();
-        // v.feed();
-        v.exercise();
+        // // v.feed();
+        // v.exercise();
     }
 }
